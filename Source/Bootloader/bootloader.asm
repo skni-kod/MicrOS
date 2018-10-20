f98f1aa368cae19c37c581ca298bbfc7d91e54c4
tearth <Tearth@users.noreply.github.com>
2018-10-20 11:11:56 +0200

    Add functions to load floppy data to the memory.

--- Source/Bootloader/bootloader.asm
+++ Source/Bootloader/bootloader.asm
@@ -24,15 +24,58 @@ VolumeID                        dd 0x12345678
 PartitionVolumeLabel            db 'PARTITION 1'    ; 11 chars
 FileSystemType                  db 'FAT12   '       ; 8 chars
 
+; Entry point of bootloader
 main:
-; Set stack pointer to be directly under bootloader
-; About 30 KiB of memory is free here
-; https://wiki.osdev.org/Memory_Map_(x86)
-mov esp, 0x7C00
-mov ax, 0
-mov ss, ax
+    ; Set stack pointer to be directly under bootloader
+    ; About 30 KiB of memory is free here
+    ; https://wiki.osdev.org/Memory_Map_(x86)
+    mov esp, 0x7C00
+    mov ax, 0
+    mov ss, ax
 
-JMP $
+    call reset_floppy
+
+    mov al, 1       ; Number of sectors to read
+    mov cl, 2       ; Sector number
+    mov bx, 0
+    mov es, bx      ; Segment
+    mov bx, 0x7E00  ; Offset
+    call load_floppy_to_memory
+
+    JMP $
+
+; Input: nothing
+; Output:
+;   - ah - disk operation status (http://stanislavs.org/helppc/int_13-1.html)
+;   - cf - 1 if successful, 0 if error
+reset_floppy:
+    ; Function name: Reset Disk System
+    mov ah, 0
+
+    int 0x13
+    ret
+
+; Input:
+;   - al - number of sectors to read
+;   - cl - sector number
+;   - es - segment
+;   - bx - offset
+; Output:
+;   - ah - status (http://stanislavs.org/helppc/int_13-1.html)
+;   - al - number of sectors read
+;   - cf - 1 if successful, 0 if error
+load_floppy_to_memory:
+    ; Function name: Read Disk Sectors
+    mov ah, 0x02
+
+    ; Track number
+    mov ch, 0
+
+    ; Head number
+    mov dh, 0
+
+    int 0x13
+    ret
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
