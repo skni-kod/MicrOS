6e3e3f2166ac6965fcc0ab6b650b07c81fb00902
tearth <Tearth@users.noreply.github.com>
2018-10-30 17:42:33 +0100

    Add a check if bootloader and kernel has been compiled with success.

--- Scripts/build.bat
+++ Scripts/build.bat
@@ -3,8 +3,20 @@ rem Build script for MicrOS. Compiles bootloader, kernel, and uploads them to th
 rem Compile bootloader
 Tools\nasm.exe Source\Bootloader\bootloader.asm -f bin -o Build\bootloader.bin
 
+rem Check if bootloader has been compiled with success
+if not exist Build\bootloader.bin (
+    echo "Bootloader error!"
+    exit
+)
+
 rem Compile kernel
 Tools\nasm.exe Source\Kernel\kernel.asm -f bin -o Build\kernel.bin
+
+rem Check if kernel has been compiled with success
+if not exist Build\kernel.bin (
+    echo "Kernel error!"
+    exit
+)
 
 rem Upload bootloader to the floppy
 Tools\dd.exe if=Build\bootloader.bin bs=512 of=Build\floppy.img
