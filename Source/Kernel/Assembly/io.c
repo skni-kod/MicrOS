3376eeec9544bf07800d8dcec381a3dd4b3ab84d
tearth <Tearth@users.noreply.github.com>
2018-11-01 21:07:57 +0100

    Add wrappers for I/O operations.

---
+++ Source/Kernel/Assembly/io.c
@@ -0,0 +1,13 @@
+#include "io.h"
+
+uint8_t inb(uint16_t port)
+{
+    uint8_t value;
+    __asm__ ("inb %1, %0" : "=a"(value) : "Nd"(port));
+    return value;
+}
+
+void outb(uint16_t port, uint8_t val)
+{
+    __asm__ ("outb %0, %1" : : "a"(val), "Nd"(port));
+}
---
+++ Source/Kernel/Assembly/io.h
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+uint8_t inb(uint16_t port);
+void outb(uint16_t port, uint8_t val);
