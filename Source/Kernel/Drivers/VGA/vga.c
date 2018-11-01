56b737aa249fdb52684b19dcced4a86a89b9f56a
AzuxDario <pawel_mibor95@tlen.pl>
2018-11-01 01:32:05 +0100

    Improve graphic driver
    New line and screen scroll, when end of screen reached, now works. Add
    colors and printing color characters.

--- Source/Kernel/Drivers/VGA/vga.c
+++ Source/Kernel/Drivers/VGA/vga.c
@@ -1,13 +1,34 @@
 #include "vga.h"
 
-int cursor_pos = 0;
+struct screen_pos cursor_pos = { .x = 0, .y = 0 };
 
 void vga_printchar(char c)
 {
-    volatile char* video = (volatile char*)(VGA_BASE_ADDR + cursor_pos);
-    *video = c;
+    vga_printcharcolor(c, 0);
+}
 
-    cursor_pos += 2;
+void vga_printcharcolor(char c, union vga_color* color)
+{
+    int pos = cursor_pos.x + cursor_pos.y * VGA_SCREEN_COLUMNS;
+    volatile union screen* video = (volatile union screen*)(VGA_BASE_ADDR);
+
+    if(c != '\n')
+    {
+        video[pos].c.code = c;
+        if(color != 0)
+        {
+            video[pos].c.color = *color;
+        }
+        cursor_pos.x += 1;
+        if(cursor_pos.x == VGA_SCREEN_COLUMNS)
+        {
+            vga_newline();
+        }
+    }
+    else
+    {
+        vga_newline();
+    }
 }
 
 void vga_printstring(char* str)
@@ -17,5 +38,38 @@ void vga_printstring(char* str)
     {
         vga_printchar(*ptr);
         ptr++;
+    }
+}
+
+void vga_newline()
+{
+    cursor_pos.x = 0;
+    cursor_pos.y++;
+    // When we reach end of screen
+    if(cursor_pos.y == VGA_SCREEN_ROWS)
+    {
+        volatile union screen* video = (volatile union screen*)(VGA_BASE_ADDR);
+        // To current line we copy next line
+        for(int i = 0; i < VGA_SCREEN_ROWS - 1; ++i)
+        {
+            // Every column has 2 bytes (character and color)
+            for(int j = 0; j < VGA_SCREEN_COLUMNS; ++j)
+            {
+                int pos = j + i * VGA_SCREEN_COLUMNS;
+                // Copy byte from next line
+                video[pos] = video[pos + VGA_SCREEN_COLUMNS];
+            }
+        }
+        cursor_pos.y = VGA_SCREEN_ROWS - 1;
+
+        // Clear last line
+        struct vga_color_without_blink col = { .background = VGA_COLOR_BLACK, .letter = VGA_COLOR_LIGHT_GRAY };
+        for(int i = 0; i < VGA_SCREEN_COLUMNS; ++i)
+        {
+            int pos = i + cursor_pos.y * VGA_SCREEN_COLUMNS; 
+            // Clear 
+            video[pos].c.code = 0;
+            video[pos].c.color.vga_color_without_blink = col;
+        }
     }
 }
