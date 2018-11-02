df69273f87e5bb728d980671d4e666e69ef3b2d5
tearth <Tearth@users.noreply.github.com>
2018-11-02 14:33:16 +0100

    Remove useless defines in PIC and add some comments.

--- Source/Kernel/Interrupts/PIC/pic.c
+++ Source/Kernel/Interrupts/PIC/pic.c
@@ -2,39 +2,47 @@
 
 void pic_init()
 {
+	// Move all interrupts from 0..7 to 32..39
+	// First parameter means that IRQ0 data will have 0x20 (32) offset in IDT
+	// Second parameter means that IRQ8 data will have 0x28 (40) offset in IDT
     pic_remap(0x20, 0x28);
 }
 
-void pic_remap(int source, int destination)
+void pic_remap(int master_offset, int slave_offset)
 {
-    unsigned char a1, a2;
+	// Save current masks because they will be destroyed during reinit
+	uint8_t master_mask = inb(MASTER_PIC_DATA);
+	uint8_t slave_mask = inb(SLAVE_PIC_DATA);
  
-	a1 = inb(PIC1_DATA);
-	a2 = inb(PIC2_DATA);
- 
-	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+	// Send initialization sequence to the master PIC and slave PIC
+	outb(MASTER_PIC_COMMAND, 0x11);
 	io_wait();
-	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+	outb(SLAVE_PIC_COMMAND, 0x11);
 	io_wait();
-	outb(PIC1_DATA, source);
-	io_wait();
-	outb(PIC2_DATA, destination);
-	io_wait();
-	outb(PIC1_DATA, 4); 
-	io_wait();
-	outb(PIC2_DATA, 2); 
-	io_wait();
- 
-	outb(PIC1_DATA, ICW4_8086);
-	io_wait();
-	outb(PIC2_DATA, ICW4_8086);
-	io_wait();
- 
-	outb(PIC1_DATA, a1);
-	outb(PIC2_DATA, a2);
-}
 
-void io_wait()
-{
-    __asm__ ( "outb %%al, $0x80" : : "a"(0) );
+	// Set master PIC offset
+	outb(MASTER_PIC_DATA, master_offset);
+	io_wait();
+
+	// Set slave PIC offset
+	outb(SLAVE_PIC_DATA, slave_offset);
+	io_wait();
+
+	// Tell master PIC that there is a slave PIC on the IRQ2 (4 = 0100)
+	outb(MASTER_PIC_DATA, 4); 
+	io_wait();
+
+	// Tell slave PIC about his role
+	outb(SLAVE_PIC_DATA, 2); 
+	io_wait();
+ 
+	// We are in 8086 mode
+	outb(MASTER_PIC_DATA, 0x01);
+	io_wait();
+	outb(SLAVE_PIC_DATA, 0x01);
+	io_wait();
+ 
+	// Restore masks for master PIC and slave PIC
+	outb(MASTER_PIC_DATA, master_mask);
+	outb(SLAVE_PIC_DATA, slave_mask);
 }
--- Source/Kernel/Interrupts/PIC/pic.h
+++ Source/Kernel/Interrupts/PIC/pic.h
@@ -1,23 +1,11 @@
 #pragma once
+#define MASTER_PIC_COMMAND 0x20
+#define MASTER_PIC_DATA 0x21
 
-#define PIC1		0x20
-#define PIC2		0xA0
-#define PIC1_COMMAND	PIC1
-#define PIC1_DATA	(PIC1+1)
-#define PIC2_COMMAND	PIC2
-#define PIC2_DATA	(PIC2+1)
+#define SLAVE_PIC_COMMAND 0xA0
+#define SLAVE_PIC_DATA 0xA1
 
-#define ICW1_ICW4	0x01
-#define ICW1_SINGLE	0x02
-#define ICW1_INTERVAL4	0x04
-#define ICW1_LEVEL	0x08
-#define ICW1_INIT	0x10
- 
-#define ICW4_8086	0x01
-#define ICW4_AUTO	0x02
-#define ICW4_BUF_SLAVE	0x08
-#define ICW4_BUF_MASTER	0x0C
-#define ICW4_SFNM	0x10
+#include <stdint.h>
 
 void pic_init();
 void io_wait();
