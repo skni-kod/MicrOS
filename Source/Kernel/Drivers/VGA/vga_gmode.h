dcf977152794d24c049d87c5b533bac60949b063
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-11-07 00:13:56 +0100

    13H Works! Working on text mode...

--- Source/Kernel/Drivers/VGA/vga_gmode.h
+++ Source/Kernel/Drivers/VGA/vga_gmode.h
@@ -6,6 +6,8 @@
 #define miscPort 0x3c2
 #define crtcIndexPort 0x3d4
 #define crtcDataPort 0x3d5
+#define crtcIndexPortText 0x3b4
+#define crtcDataPortText 0x3b5
 #define sequencerIndexPort 0x3c4
 #define sequencerDataPort 0x3c5
 #define graphicsControllerIndexPort 0x3ce
@@ -16,8 +18,12 @@
 #define attributeControllerResetPort 0x3da
 
 void writeRegisters(uint8_t* registers);
+void writeRegistersText(uint8_t* registers);
 void set13HVideoMode();
+void set3HVideoMode();
 void pixel_256 (unsigned char color, unsigned int x, unsigned int y);
 void drawDupaIn13H(int color);
+
+void setFont(uint8_t* buf, uint8_t font_height);
 
 #endif
