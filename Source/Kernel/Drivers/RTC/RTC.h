46e3976582ecda9e2b59b0797140db90ff5a586d
Paweł Dziedzic <blavikenn@gmail.com>
2018-11-06 22:00:50 +0100

    Added splitted read functions

--- Source/Kernel/Drivers/RTC/RTC.h
+++ Source/Kernel/Drivers/RTC/RTC.h
@@ -4,14 +4,22 @@
 #include <stdint.h>
 #include "../../Assembly/io.h"
 
-unsigned char second;
-unsigned char minute;
-unsigned char hour;
-unsigned char day;
-unsigned char month;
-unsigned int year;
+typedef struct rtc_time
+{
+    unsigned char second;
+    unsigned char minute;
+    unsigned char hour;
+    unsigned char day;
+    unsigned char month;
+    unsigned int year;
+} rtc_time;
 
-void read_rtc();
-
+void read_rtc(rtc_time* time);
+uint8_t read_second();
+uint8_t read_minute();
+uint8_t read_hour();
+uint8_t read_day();
+uint8_t read_month();
+uint32_t read_year();
 
 #endif
