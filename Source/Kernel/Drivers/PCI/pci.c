76b94b99547253cdf71be197d601dc73e858804a
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-11-12 01:32:03 +0100

    Tryging debuging stuff

--- Source/Kernel/Drivers/PCI/pci.c
+++ Source/Kernel/Drivers/PCI/pci.c
@@ -1,5 +1,6 @@
 #include "pci.h"
 #include "../../Assembly/io.h"
+#include "../../Logger/logger.h"
 
 pci_dev devices[32];
 uint8_t number_of_devices = 0;
@@ -31,11 +32,19 @@ uint8_t get_number_of_devices()
 void pci_init()
 {
     pci_in_data data;
+    data.enable = 1;
+    char str[7] = "000 00";
     for(uint16_t bus = 0; bus < 256; bus++)
     {
         data.bus_num = bus;
+        str[0] = (bus/ 100) + 48;
+        str[1] = ((bus % 100)/10) + 48;
+        str[2] = (bus % 10) + 48;
         for(uint8_t device=0; device < 32; device++)
         {
+            str[4] = (device/10)+48;
+            str[5] = (device%10)+48;
+            log_info(str);
             data.device_num = device;
             pci_dev dev = get_device_info(data);
             if(dev.vendor_id != 0xFFFF)
