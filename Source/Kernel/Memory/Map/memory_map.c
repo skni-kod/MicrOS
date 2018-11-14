c345f75873d642d55f4574d6321467bdeaf063b6
tearth <Tearth@users.noreply.github.com>
2018-11-14 18:33:46 +0100

    Add function to dump memory map on console.

--- Source/Kernel/Memory/Map/memory_map.c
+++ Source/Kernel/Memory/Map/memory_map.c
@@ -4,5 +4,37 @@ memory_map_entry* memory_map = MEMORY_MAP_ADDRESS;
 
 void memory_map_dump()
 {
+    
 
+    for(int i=0; i<10; i++)
+    {
+        char buffer[40];
+
+        itoa(memory_map[i].base_address >> 32, buffer, 16);
+        vga_printstring(buffer);
+
+        itoa(memory_map[i].base_address & 0xffffffff, buffer, 16);
+        vga_printstring(buffer);
+        
+        vga_printstring(" ");
+        
+
+        itoa(memory_map[i].length >> 32, buffer, 16);
+        vga_printstring(buffer);
+
+        itoa(memory_map[i].length & 0xffffffff, buffer, 16);
+        vga_printstring(buffer);
+        
+        vga_printstring(" ");
+
+        itoa(memory_map[i].type, buffer, 10);
+        vga_printstring(buffer);
+        
+        vga_printstring(" ");
+
+        itoa(memory_map[i].acpi, buffer, 10);
+        vga_printstring(buffer);
+
+        vga_newline();
+    }
 }
--- Source/Kernel/Memory/Map/memory_map.h
+++ Source/Kernel/Memory/Map/memory_map.h
@@ -1,8 +1,10 @@
 #ifndef MEMORY_MAP_H
 #define MEMORY_MAP_H
 
-#define MEMORY_MAP_ADDRESS 0x0500
+#define MEMORY_MAP_ADDRESS 0xc0000500
 
+#include "../../Drivers/VGA/vga.h"
+#include "../../../../Library/stdlib.h"
 #include "memory_map_entry.h"
 
 void memory_map_dump();
