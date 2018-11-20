c80b30c71f6a892f89c1638389d6dbe2bc369594
tearth <Tearth@users.noreply.github.com>
2018-11-20 23:09:17 +0100

    Add memory guard for stack.

--- Source/Kernel/Memory/Paging/paging.c
+++ Source/Kernel/Memory/Paging/paging.c
@@ -6,9 +6,19 @@ paging_table_entry* page_tables = (paging_table_entry*)PAGE_TABLES_ADDRESS;
 void paging_init()
 {
     paging_remove_identity();
+    paging_add_stack_guard();
 }
 
 void paging_remove_identity()
 {
     page_directory[0].present = 0;
+}
+
+void paging_add_stack_guard()
+{
+    paging_table_entry page_definition_with_stack = page_directory[768 + 4];
+    uint32_t* page_address = (page_definition_with_stack.physical_page_address << 12) + 0xC0000000;
+
+    paging_table_entry* page_with_stack = page_address;
+    page_with_stack->present = 0;
 }
--- Source/Kernel/Memory/Paging/paging.h
+++ Source/Kernel/Memory/Paging/paging.h
@@ -1,12 +1,13 @@
 #ifndef PAGING_H
 #define PAGING_H
 
-#define PAGE_DIRECTORY_ADDRESS 0x200000
-#define PAGE_TABLES_ADDRESS 0x201000
+#define PAGE_DIRECTORY_ADDRESS 0xC0001000
+#define PAGE_TABLES_ADDRESS 0xC1100000
 
 #include "paging_table_entry.h"
 
 void paging_init();
 void paging_remove_identity();
+void paging_add_stack_guard();
 
 #endif
