d7653aef339a349395e7da0aac050b5c68a5b4c9
tearth <Tearth@users.noreply.github.com>
2018-11-23 18:04:30 +0100

    Add empty function to the memory management in the library.

--- Library/stdlib.c
+++ Library/stdlib.c
@@ -76,3 +76,23 @@ char *itoa(int input, char *buffer, int base)
 
     return buffer;
 }
+
+void* malloc(size_t size)
+{
+    
+}
+
+void* calloc(size_t num, size_t size)
+{
+
+}
+
+void* realloc(void* ptr, size_t size)
+{
+
+}
+
+void free(void* ptr)
+{
+
+}
--- Library/stdlib.h
+++ Library/stdlib.h
@@ -10,4 +10,9 @@ void sleep(uint32_t ms);
 int32_t atoi(const char* string);
 char* itoa(int input, char* buffer, int base);
 
+void* malloc(size_t size);
+void* calloc(size_t num, size_t size);
+void* realloc(void* ptr, size_t size);
+void free(void* ptr);
+
 #endif
