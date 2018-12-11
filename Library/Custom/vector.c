89fd960c4f0664004e2fb2c0be2ff2dced3b813a
tearth <Tearth@users.noreply.github.com>
2018-12-12 00:01:25 +0100

    Refactoring - documents formatting.

--- Library/Custom/vector.c
+++ Library/Custom/vector.c
@@ -1,29 +1,29 @@
 #include "vector.h"
 
-void vector_init(vector* vector)
+void vector_init(vector *vector)
 {
 	vector->data = 0;
 	vector->size = 0;
 	vector->count = 0;
 }
 
-void vector_add(vector* vector, void* data)
+void vector_add(vector *vector, void *data)
 {
 	vector_insert(vector, data, vector->count);
 }
 
-void vector_insert(vector* vector, void* data, uint32_t index)
+void vector_insert(vector *vector, void *data, uint32_t index)
 {
 	if (vector->size == 0)
 	{
 		vector->size = 8;
-		vector->data = malloc(sizeof(void*) * vector->size);
+		vector->data = malloc(sizeof(void *) * vector->size);
 	}
 
 	if (vector->count + 1 > vector->size)
 	{
 		vector->size *= 2;
-		vector->data = realloc(vector->data, sizeof(void*) * vector->size);
+		vector->data = realloc(vector->data, sizeof(void *) * vector->size);
 	}
 
 	for (uint32_t i = vector->count; i >= index; i--)
@@ -35,7 +35,7 @@ void vector_insert(vector* vector, void* data, uint32_t index)
 	vector->count++;
 }
 
-void vector_remove(vector* vector, uint32_t index)
+void vector_remove(vector *vector, uint32_t index)
 {
 	if (index >= vector->count)
 	{
@@ -50,11 +50,11 @@ void vector_remove(vector* vector, uint32_t index)
 	vector->count--;
 }
 
-void vector_clear(vector* vector)
+void vector_clear(vector *vector)
 {
 	if (vector->size > 0)
 	{
-		for(uint32_t i=0; i<vector->count; i++)
+		for (uint32_t i = 0; i < vector->count; i++)
 		{
 			free(vector->data[i]);
 		}
--- Library/Helpers/interrupt_helper.c
+++ Library/Helpers/interrupt_helper.c
@@ -3,13 +3,13 @@
 uint32_t call_interrupt_0a(uint32_t function_number)
 {
     uint32_t result;
-    __asm__ (
-        "mov %1, %%eax\n" \
-        "int $48\n" \
+    __asm__(
+        "mov %1, %%eax\n"
+        "int $48\n"
         "mov %%eax, %0\n"
 
-        : "=g" (result)
-        : "g" (function_number)
+        : "=g"(result)
+        : "g"(function_number)
         : "%eax");
 
     return result;
@@ -18,14 +18,14 @@ uint32_t call_interrupt_0a(uint32_t function_number)
 uint32_t call_interrupt_1a(uint32_t function_number, uint32_t a)
 {
     uint32_t result;
-    __asm__ (
-        "mov %1, %%eax\n" \
-        "mov %2, %%ebx\n" \
-        "int $48\n" \
+    __asm__(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "int $48\n"
         "mov %%eax, %0\n"
 
-        : "=g" (result)
-        : "g" (function_number), "g" (a)
+        : "=g"(result)
+        : "g"(function_number), "g"(a)
         : "%eax", "%ebx");
 
     return result;
@@ -34,15 +34,15 @@ uint32_t call_interrupt_1a(uint32_t function_number, uint32_t a)
 uint32_t call_interrupt_2a(uint32_t function_number, uint32_t a, uint32_t b)
 {
     uint32_t result;
-    __asm__ (
-        "mov %1, %%eax\n" \
-        "mov %2, %%ebx\n" \
-        "mov %3, %%ecx\n" \
-        "int $48\n" \
+    __asm__(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "mov %3, %%ecx\n"
+        "int $48\n"
         "mov %%eax, %0\n"
 
-        : "=g" (result)
-        : "g" (function_number), "g" (a), "g" (b)
+        : "=g"(result)
+        : "g"(function_number), "g"(a), "g"(b)
         : "%eax", "%ebx", "%ecx");
 
     return result;
@@ -51,16 +51,16 @@ uint32_t call_interrupt_2a(uint32_t function_number, uint32_t a, uint32_t b)
 uint32_t call_interrupt_3a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c)
 {
     uint32_t result;
-    __asm__ (
-        "mov %1, %%eax\n" \
-        "mov %2, %%ebx\n" \
-        "mov %3, %%ecx\n" \
-        "mov %4, %%edx\n" \
-        "int $48\n" \
+    __asm__(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "mov %3, %%ecx\n"
+        "mov %4, %%edx\n"
+        "int $48\n"
         "mov %%eax, %0\n"
 
-        : "=g" (result)
-        : "g" (function_number), "g" (a), "g" (b), "g" (c)
+        : "=g"(result)
+        : "g"(function_number), "g"(a), "g"(b), "g"(c)
         : "%eax", "%ebx", "%ecx", "%edx");
 
     return result;
