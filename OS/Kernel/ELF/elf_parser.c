97a2ddf44d38e392f4e875dd3ac6f4c7027cc304
tearth <Tearth@users.noreply.github.com>
2018-12-13 23:26:59 +0100

    Add function to parse section header.

--- OS/Kernel/ELF/elf_parser.c
+++ OS/Kernel/ELF/elf_parser.c
@@ -10,3 +10,9 @@ elf_program_header *elf_get_program_header(uint8_t *content)
     elf_header *header = elf_get_header(content);
     return (elf_program_header *)(content + header->program_header_table_position);
 }
+
+elf_section_header *elf_get_section_header_with_index(uint8_t *content, uint8_t index)
+{
+    elf_header *header = elf_get_header(content);
+    return (elf_section_header *)(content + header->section_header_table_position + (index * sizeof(elf_section_header)));
+}
--- OS/Kernel/ELF/elf_parser.h
+++ OS/Kernel/ELF/elf_parser.h
@@ -3,8 +3,10 @@
 
 #include "elf_header.h"
 #include "elf_program_header.h"
+#include "elf_section_header.h"
 
 elf_header *elf_get_header(uint8_t *content);
 elf_program_header *elf_get_program_header(uint8_t *content);
+elf_section_header *elf_get_section_header_with_index(uint8_t *content, uint8_t index);
 
 #endif
