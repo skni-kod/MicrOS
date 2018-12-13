3ffadb33eeb726a06411e17ebd31a4b90f944c11
tearth <Tearth@users.noreply.github.com>
2018-12-13 20:00:42 +0100

    Add initial functions to parse section header.

--- OS/Kernel/ELF/elf_parser.c
+++ OS/Kernel/ELF/elf_parser.c
@@ -4,3 +4,15 @@ elf_header *elf_parse(uint8_t *content)
 {
     return (elf_header *)content;
 }
+
+elf_section_header *elf_parse_text_header(uint8_t *content)
+{
+    elf_header *header = elf_parse(content);
+    return (elf_section_header *)(content + header->program_header_table_position);
+}
+
+elf_section_header *elf_parse_data_header(uint8_t *content)
+{
+    elf_header *header = elf_parse(content);
+    return (elf_section_header *)(content + header->section_header_table_position);
+}
--- OS/Kernel/ELF/elf_parser.h
+++ OS/Kernel/ELF/elf_parser.h
@@ -2,7 +2,10 @@
 #define ELF_PARSER_H
 
 #include "elf_header.h"
+#include "elf_section_header.h"
 
 elf_header *elf_parse(uint8_t *content);
+elf_section_header *elf_parse_text_header(uint8_t *content);
+elf_section_header *elf_parse_data_header(uint8_t *content);
 
 #endif
