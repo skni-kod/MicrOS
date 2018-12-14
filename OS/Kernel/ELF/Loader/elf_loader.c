b2c40e76105a848a8b653415e27ffaa62812437e
tearth <Tearth@users.noreply.github.com>
2018-12-14 17:26:02 +0100

    Add working ELF loader.

--- OS/Kernel/ELF/Loader/elf_loader.c
+++ OS/Kernel/ELF/Loader/elf_loader.c
@@ -1,1 +1,27 @@
 #include "elf_loader.h"
+
+void elf_loader_load(uint8_t *content)
+{
+    elf_header *header = elf_get_header(content);
+    elf_program_header *program_header = elf_get_program_header(content);
+
+    uint32_t size_in_memory = elf_get_total_size_in_memory(content);
+    uint32_t pages_count = (size_in_memory / 1024 / 1024 / 4) + 1;
+
+    uint32_t initial_page = header->entry_position / 1024 / 1024 / 4;
+
+    for (int i = 0; i < pages_count; i++)
+    {
+        uint32_t physical_address = physical_memory_alloc_page();
+        paging_map_page(physical_address, (header->entry_position / 1024 / 1024 / 4) + i);
+    }
+
+    for (int i = 0; i < header->section_header_table_entries_in_section_count; i++)
+    {
+        elf_section_header *section_header = elf_get_section_header_with_index(content, i);
+        if (section_header->virtual_address != 0)
+        {
+            memcpy(section_header->virtual_address, content + section_header->file_offset, section_header->size);
+        }
+    }
+}
--- OS/Kernel/ELF/Loader/elf_loader.h
+++ OS/Kernel/ELF/Loader/elf_loader.h
@@ -2,5 +2,9 @@
 #define ELF_LOADER_H
 
 #include "../Parser/elf_parser.h"
+#include "../../Memory/Paging/paging.h"
+#include "../../Memory/Manager/Physic/physical_memory_manager.h"
+
+void elf_loader_load(uint8_t *content);
 
 #endif
