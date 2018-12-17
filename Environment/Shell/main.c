1e1b22336250f6493fe68e203f527ef794256dbd
tearth <Tearth@users.noreply.github.com>
2018-12-17 14:29:35 +0100

    Update kmain and example app.

--- Environment/Shell/main.c
+++ Environment/Shell/main.c
@@ -1,21 +1,12 @@
 #include <stdint.h>
+#include <time.h>
 
 int test_array[1024];
 
 int main()
 {
-    /*
-    uint8_t *vga = (uint8_t *)0xc00b8000;
+    uint32_t test_clock = clock();
 
-    for (int i = 0; i < 1024; i++)
-    {
-        test_array[i] = i;
-    }
-
-    for (int i = 0; i < 1024; i++)
-    {
-        vga[i * 2] = test_array[i];
-    }*/
     while (1)
         ;
 }
