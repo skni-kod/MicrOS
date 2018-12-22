965d1bf159fb058be16b8392e4f60bda71e1d382
tearth <Tearth@users.noreply.github.com>
2018-12-22 19:46:59 +0100

    Update test process.

--- Environment/Shell/main.c
+++ Environment/Shell/main.c
@@ -1,22 +1,31 @@
 #include <stdint.h>
 #include <time.h>
 #include <stdlib.h>
+#include <time.h>
 
 int test_array[1024];
 
-int main()
+int main(int id)
 {
-    uint32_t start_time;
-    start_time = clock();
+    uint32_t asd = 100;
+    for (int i = 0; i < id; i++)
+    {
+        asd *= 10;
+    }
 
     while (1)
     {
-        int asd = clock();
+        asd++;
 
         char buffer[32];
+        call_interrupt_1a(0x04, "Process ");
+        itoa(id, buffer, 10);
+        call_interrupt_1a(0x04, buffer);
+        call_interrupt_1a(0x04, ": ");
         itoa(asd, buffer, 10);
-        call_interrupt_1a(0x04, "Process 1: ");
         call_interrupt_1a(0x04, buffer);
         call_interrupt_1a(0x04, "\n");
+
+        sleep(30);
     }
 }
