127b5a316f92f92dc9c29dba89977a3a93cbbc6c
tearth <Tearth@users.noreply.github.com>
2018-12-29 20:39:54 +0100

    Add new interrupt helpers to support functions with 4 and 5 parameters.

--- Library/MicrOS/micros_interrupts.c
+++ Library/MicrOS/micros_interrupts.c
@@ -65,3 +65,42 @@ uint32_t micros_interrupt_3a(uint32_t function_number, uint32_t a, uint32_t b, u
 
     return result;
 }
+
+uint32_t micros_interrupt_4a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c, uint32_t d)
+{
+    uint32_t result;
+    __asm__(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "mov %3, %%ecx\n"
+        "mov %4, %%edx\n"
+        "mov %5, %%esi\n"
+        "int $50\n"
+        "mov %%eax, %0\n"
+
+        : "=g"(result)
+        : "g"(function_number), "g"(a), "g"(b), "g"(c), "g"(d)
+        : "%eax", "%ebx", "%ecx", "%edx", "%esi");
+
+    return result;
+}
+
+uint32_t micros_interrupt_5a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e)
+{
+    uint32_t result;
+    __asm__(
+        "mov %1, %%eax\n"
+        "mov %2, %%ebx\n"
+        "mov %3, %%ecx\n"
+        "mov %4, %%edx\n"
+        "mov %5, %%esi\n"
+        "mov %6, %%edi\n"
+        "int $50\n"
+        "mov %%eax, %0\n"
+
+        : "=g"(result)
+        : "g"(function_number), "g"(a), "g"(b), "g"(c), "g"(d), "g"(e)
+        : "%eax", "%ebx", "%ecx", "%edx", "%esi", "%edi");
+
+    return result;
+}
--- Library/MicrOS/micros_interrupts.h
+++ Library/MicrOS/micros_interrupts.h
@@ -7,5 +7,7 @@ uint32_t micros_interrupt_0a(uint32_t function_number);
 uint32_t micros_interrupt_1a(uint32_t function_number, uint32_t a);
 uint32_t micros_interrupt_2a(uint32_t function_number, uint32_t a, uint32_t b);
 uint32_t micros_interrupt_3a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c);
+uint32_t micros_interrupt_4a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c, uint32_t d);
+uint32_t micros_interrupt_5a(uint32_t function_number, uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e);
 
 #endif
