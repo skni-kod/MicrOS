b10121290247fdd7faaf96afe20ca7113998b1c8
tearth <tearthdev@gmail.com>
2019-02-07 02:08:20 +0100

    Add support for quotation marks in program parameters.

--- Library/GCC/crt0.c
+++ Library/GCC/crt0.c
@@ -11,17 +11,22 @@ void _start(char *path, char *parameters)
 char **parse_parameters(char *path, char *parameters, int *count)
 {
     char **argv = (char **)malloc(sizeof(char *) * 2);
+    char *current_string = (char *)malloc(sizeof(char));
     char *parameteres_ptr = parameters;
+    int current_string_length = 1;
+
+    bool singleQuotationMarks = false;
+    bool doubleQuotationMarks = false;
 
     argv[0] = path;
     *count = 2;
 
-    char *current_string = malloc(sizeof(char));
-    int current_string_length = 1;
-
     while (true)
     {
-        if (*parameteres_ptr == ' ' || *parameteres_ptr == 0)
+        if ((*parameteres_ptr == 0) ||
+            (*parameteres_ptr == ' ' && !singleQuotationMarks && !doubleQuotationMarks) ||
+            (singleQuotationMarks && *parameteres_ptr == '\'') ||
+            (doubleQuotationMarks && *parameteres_ptr == '\"'))
         {
             current_string[current_string_length - 1] = 0;
 
@@ -36,6 +41,22 @@ char **parse_parameters(char *path, char *parameters, int *count)
 
             current_string = malloc(sizeof(char));
             current_string_length = 1;
+
+            if (singleQuotationMarks || doubleQuotationMarks)
+            {
+                singleQuotationMarks = false;
+                doubleQuotationMarks = false;
+
+                parameteres_ptr++;
+            }
+        }
+        else if (*parameteres_ptr == '\'' && !singleQuotationMarks && !doubleQuotationMarks)
+        {
+            singleQuotationMarks = true;
+        }
+        else if (*parameteres_ptr == '\"' && !singleQuotationMarks && !doubleQuotationMarks)
+        {
+            doubleQuotationMarks = true;
         }
         else
         {
