402040f29f24248abcdb2f94ea5ee31e116be94f
Paweł Dziedzic <blavikenn@gmail.com>
2019-03-07 22:38:40 +0100

    Simple implementation of sprintf (%s doesn't work at this point)

--- Library/stdio.c
+++ Library/stdio.c
@@ -1,9 +1,99 @@
-int sprintf(char *str, const char *format, ...)
+#include <stdarg.h>
+#include "stdlib.h"
+
+int sscanf(const char *str, const char *format, ...)
 {
     return 0;
 }
 
-int sscanf(const char *str, const char *format, ...)
+int sprintf(char *str, const char *format, ...)
 {
+    va_list arg;
+    va_start(arg, format);
+
+    int int_arg;
+    char *str_arg;
+    unsigned int put_index = 0;
+
+    char *traverse;
+    for (traverse = format; *traverse != '\0'; traverse++)
+    {
+
+        // put char to buffer until you see '%'
+        while (*traverse != '%')
+        {
+            str[put_index++] = *traverse;
+
+            ++traverse;
+        }
+
+        // set pointer to character after '%'
+        ++traverse;
+
+#define BUFFER_SIZE 16
+
+        char buffer[BUFFER_SIZE];
+        int index = 0;
+        switch (*traverse)
+        {
+        case 'c':
+            int_arg = va_arg(arg, int);
+            str[put_index++] = int_arg;
+            break;
+
+        case 'd':
+            int_arg = va_arg(arg, int);
+
+            // convert argument to string and put to buffer
+            itoa(int_arg, buffer, 10);
+            index = 0;
+            while (buffer[index] != '\0' && index < BUFFER_SIZE)
+            {
+                str[put_index++] = buffer[index];
+                index++;
+            }
+            break;
+
+        case 'o':
+            int_arg = va_arg(arg, unsigned int);
+            // convert argumnrnt to string with 8 as base and put to buffer
+            itoa(int_arg, buffer, 8);
+            index = 0;
+            while (buffer[index] != '\0' && index < BUFFER_SIZE)
+            {
+                str[put_index++] = buffer[index];
+                index++;
+            }
+            break;
+
+        case 's':
+            // str_arg = va_arg(arg, char *);
+            // // put arg to buffer
+            // index = 0;
+            // while (str_arg[index] != '\0')
+            // {
+            //     str[put_index++] = str_arg[index];
+            //     index++;
+            // }
+            break;
+
+        case 'x':
+            int_arg = va_arg(arg, unsigned int);
+            // convert arg to string with 16 as base and put to buffer
+            itoa(int_arg, buffer, 16);
+            index = 0;
+            while (buffer[index] != '\0' && index < BUFFER_SIZE)
+            {
+                str[put_index++] = buffer[index];
+                index++;
+            }
+
+            break;
+        }
+    }
+
+    str[put_index++] = '\0';
+
+    va_end(arg);
     return 0;
 }
