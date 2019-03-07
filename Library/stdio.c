f1c4e1c0ea010c446fd89620bfff8413a5a9bbaa
Paweł Dziedzic <blavikenn@gmail.com>
2019-03-07 20:36:41 +0100

    add sscanf and sprintf prototypes

---
+++ Library/stdio.c
@@ -0,0 +1,9 @@
+int sprintf(char *str, const char *format, ...)
+{
+    return 0;
+}
+
+int sscanf(const char *str, const char *format, ...)
+{
+    return 0;
+}
---
+++ Library/stdio.h
@@ -0,0 +1,3 @@
+int sprintf(char* str, const char* format, ...);
+
+int sscanf(const char *str, const char *format, ...);
