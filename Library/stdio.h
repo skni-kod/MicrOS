0c9c46b48289f910b5c47fa81c52c853ef6f2927
Paweł Dziedzic <blavikenn@gmail.com>
2019-03-08 22:49:06 +0100

    Fix typo

--- Library/stdio.h
+++ Library/stdio.h
@@ -1,8 +1,8 @@
-#ifndef SRDIO_H
+#ifndef STDIO_H
 #define STDIO_H
 
 int sprintf(char* str, const char* format, ...);
 
 int sscanf(const char *str, const char *format, ...);
 
-#endif
+#endif //STDIO_H
