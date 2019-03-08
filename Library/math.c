c365cb234347a6eff9abe1b7054c19d5b070501a
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-08 23:09:48 +0100

    Fix round lround and llround functions
    They should rounding to nearest even value, which couldn't be done if rounding direction would be different than nearest.

--- Library/math.c
+++ Library/math.c
@@ -609,65 +609,113 @@ float truncf(float x)
 
 double round(double x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     __asm__ (
         "fldl %1 \n" \
         "frndint \n" \
         "fstpl %0"
         : "=m"(x): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
 float roundf(float x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     __asm__ (
         "fld %1 \n" \
         "frndint \n" \
         "fstp %0"
         : "=m"(x): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
 long int lround(double x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     long int result;
     __asm__ (
         "fldl %1 \n" \
         "frndint \n" \
         "fistp %0"
         : "=m"(result): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
 long int lroundf(float x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     long int result;
     __asm__ (
         "fld %1 \n" \
         "frndint \n" \
         "fistp %0"
         : "=m"(result): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
 long long int llround(double x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     long long int result;
     __asm__ (
         "fldl %1 \n" \
         "frndint \n" \
         "fistp %0"
         : "=m"(result): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
 long long int llroundf(float x)
 {
+    // Change FPU control to nearest
+    fcontrol_t previous, current;
+    previous = _FPU_read_control_word();
+    current = previous;
+    current.rounding_control = _FPU_ROUNDING_NEAREST_EVEN;
+    _FPU_write_control_word(current);
     long long int result;
     __asm__ (
         "fld %1 \n" \
         "frndint \n" \
         "fistp %0"
         : "=m"(result): "m"(x));
+    // Restore previous FPU control
+    _FPU_write_control_word(previous);
     return x;
 }
 
