cdd706b66c5fbf594d5178906ce6d6f88515c83b
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-09 00:40:09 +0100

    repair isalpha

--- Library/ctype/isalpha.c
+++ Library/ctype/isalpha.c
@@ -1,8 +1,8 @@
 #include "ctype.h"
 
-int isalnum(int c)
+int isalpha(int c)
 {
-    if((c >= 0x61 && c <= 0x7a) || (c >= 0x41 && c <= 0x5a) || (c >= 0x30 && c <= 0x39))
+    if((c >= 0x61 && c <= 0x7a) || (c >= 0x41 && c <= 0x5a))
         return 1;
     else
         return 0;
