f69cef968e305d6ba74f7ea74688c97ee8f57d91
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-09 13:00:55 +0100

    Fix copysign and copysignf

--- Library/math.c
+++ Library/math.c
@@ -857,15 +857,16 @@ double copysign(double x, double y)
     unsigned char sign_of_x = signbit(x);
     unsigned char sign_of_y = signbit(y);
     double * p_x = &x;
+    unsigned char * ucharp_x = (unsigned char *)p_x;
     if(sign_of_x != sign_of_y)
     {
         if(sign_of_x == 0)
         {
-            ((unsigned char *)(p_x))[0] = (((unsigned char )(p_x)[0]) & (unsigned char)255);
+            ucharp_x[sizeof(double) - 1] = ucharp_x[sizeof(double) - 1] | 128;
         }
         else
         {
-            ((unsigned char *)(p_x))[0] = (((unsigned char)(p_x)[0]) & (unsigned char)127);
+            ucharp_x[sizeof(double) - 1] = ucharp_x[sizeof(double) - 1] & 127;
         }
     }
     return x;
@@ -876,15 +877,16 @@ float copysignf(float x, float y)
     unsigned char sign_of_x = signbit(x);
     unsigned char sign_of_y = signbit(y);
     float * p_x = &x;
+    unsigned char * ucharp_x = (unsigned char *)p_x;
     if(sign_of_x != sign_of_y)
     {
         if(sign_of_x == 0)
         {
-            ((unsigned char *)(p_x))[0] = (((unsigned char )(p_x)[0]) & (unsigned char)255);
+            ucharp_x[sizeof(float) - 1] = ucharp_x[sizeof(float) - 1] | 128;
         }
         else
         {
-            ((unsigned char *)(p_x))[0] = (((unsigned char)(p_x)[0]) & (unsigned char)127);
+            ucharp_x[sizeof(float) - 1] = ucharp_x[sizeof(float) - 1] & 127;
         }
     }
     return x;
