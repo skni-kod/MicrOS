5ec785fe25c13533d48d0a5e9834d65d03d60b34
tearth <tearthdev@gmail.com>
2019-03-09 22:25:58 +0100

    Fix invalid implementation of fopen and freopen.

--- Library/stdio/fopen.c
+++ Library/stdio/fopen.c
@@ -5,7 +5,11 @@ extern FILE *streams_set_stream_as_file(const char *filename, const char *mode, 
 FILE *fopen(const char *filename, const char *mode)
 {
     FILE *stream = __stdio_create_stream();
-    streams_set_stream_as_file(filename, mode, stream);
+    if (!streams_set_stream_as_file(filename, mode, stream))
+    {
+        free(stream);
+        return NULL;
+    }
 
     return stream;
 }
--- Library/stdio/freopen.c
+++ Library/stdio/freopen.c
@@ -4,5 +4,10 @@ extern FILE *streams_set_stream_as_file(const char *, const char *, FILE *);
 
 FILE *freopen(const char *filename, const char *mode, FILE *stream)
 {
-    return streams_set_stream_as_file(filename == NULL ? stream->filename : filename, mode, stream);
+    if (!streams_set_stream_as_file(filename == NULL ? stream->filename : filename, mode, stream))
+    {
+        return NULL;
+    }
+
+    return stream;
 }
