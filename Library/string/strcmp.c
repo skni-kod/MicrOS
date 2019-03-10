6e8413120179990808ff26aa63eb6f6939ad3e5f
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-10 09:20:43 +0100

    repaired strcmp.c

--- Library/string/strcmp.c
+++ Library/string/strcmp.c
@@ -2,7 +2,6 @@
 
 int strcmp(const char *str1, const char *str2)
 {
-    {
     while(*str1 && (*str1==*str2))
         str1++,str2++;
     return *(const unsigned char*)str1-*(const unsigned char*)str2;
