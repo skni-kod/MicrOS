25594fc6628e154004dd3394a7a2832b59d7b8dc
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-11 22:30:14 +0100

    Change in __math_INFl

--- Library/math/__math_INFl.c
+++ Library/math/__math_INFl.c
@@ -3,7 +3,7 @@
 long double __math_INFl()
 {
     long_double_byte f;
-    f.bytes[sizeof(long double) - 1] = 0x7F;
-    f.bytes[sizeof(long double) - 2] = 0xFF;
+    f.bytes[sizeof(long double) - 3] = 0x7F;
+    f.bytes[sizeof(long double) - 4] = 0xFF;
     return f.f;
 }
