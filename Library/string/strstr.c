7c17db1569c45e433978fc320cfb39cc9e58d4e3
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-13 21:03:09 +0100

    better comment for strstr

--- Library/string/strstr.c
+++ Library/string/strstr.c
@@ -6,8 +6,9 @@ char *strstr(char *str1, const char *str2)
     int i;
     while(*str1 != '\0')
     {
-        for(i=0 ; *str1 == *str2 && *str2 != '\0' ; str1++ , str2++)
-            i++; // magic for dont touch
+        //*******************************************************
+        for(i=0 ; *str1 == *str2 && *str2 != '\0' ; str1++ , str2++ , i++) // magic for dont touch
+        //*******************************************************
         if(*str2 == '\0')
             return ret = str1-i;
         else
