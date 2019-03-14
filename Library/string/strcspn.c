1b97a5cc24981db1a8a0cb01d9cbb1c5392db8f1
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-14 23:07:58 +0100

    Add strcspn.c strspn.c

--- Library/string/strcspn.c
+++ Library/string/strcspn.c
@@ -2,5 +2,13 @@
 
 size_t strcspn(const char *str1, const char *str2)
 {
-    return 0;
+    size_t ret = 0;
+    while(*str1)
+    {
+        if(*strchr(str2,*str1))
+            return ret;
+         else
+            str1++,ret++;
+    }
+    return ret;
 }
