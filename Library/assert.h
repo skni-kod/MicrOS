5e92f2f34a5ef6d48259a26a327587781129dd83
tearth <tearthdev@gmail.com>
2019-03-15 12:52:47 +0100

    Add initial implementation of assertion.

--- Library/assert.h
+++ Library/assert.h
@@ -1,4 +1,15 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
+#include "stdio.h"
+
+// TODO: add fprintf in format "Assertion failed: expression, file filename, line line number"
+#define STR(x) #x
+#define assert(expression)                   \
+    if (!(expression))                       \
+    {                                        \
+        fputs("Assertion failed: ", stderr); \
+        abort();                             \
+    }
+
 #endif
