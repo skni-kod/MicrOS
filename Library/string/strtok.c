9fea96aa05e4c061f129fd31d72b19ae7d587e86
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-18 12:12:19 +0100

    Add strtok.c

--- Library/string/strtok.c
+++ Library/string/strtok.c
@@ -2,5 +2,15 @@
 
 char *strtok(char *str, const char *delimiters)
 {
-    return 0;
+    static char* p=0;
+    if(str)
+        p=str;
+    else if(!p)
+        return 0;
+    str=p+strspn(p,delimiters);
+    p=str+strcspn(str,delimiters);
+    if(p==str)
+        return p=0;
+    p = *p ? *p=0,p+1 : 0;
+    return str;
 }
