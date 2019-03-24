758295c9d275c2ff5cc5bbe50584d4ad08ea7b6b
Suzurri <44876347+Suzurri@users.noreply.github.com>
2019-03-24 11:01:38 +0100

    Add memchr.c

--- Library/string/memchr.c
+++ Library/string/memchr.c
@@ -2,5 +2,14 @@
 
 void *memchr(void *ptr, int value, size_t num)
 {
+    unsigned char *p = (unsigned char*)ptr;
+    while(n)
+    {
+        if(*p != (unsigned char)value);
+            p++;
+        else
+            return p;
+        n--;
+    }
     return 0;
 }
