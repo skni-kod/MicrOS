b57c581ff94ee18a2852ae20b45d69b83b7dcb48
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-28 21:14:24 +0100

    Add more error handling

--- Library/math/expm1f.c
+++ Library/math/expm1f.c
@@ -2,5 +2,21 @@
 
 float expm1f(float x)
 {
-    return powf(M_E, x) - 1;
+    float result = powf(M_E, x) - 1;
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.overflow == 0)
+    {
+        return result;
+    }
+    // range error
+    else
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_OVERFLOW);
+        }
+        // In other case overflow is already set so we don't need to cover _math_errhandling == MATH_ERREXCEPT
+        return x > 0 ? HUGE_VALF : copysignf(HUGE_VALF, -1.0);
+    }
 }
