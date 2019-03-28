b13d26880bc90ec1f321a776cfd80cf75ffd108d
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-28 21:34:04 +0100

    Add error handling to scalbln and scalbn

--- Library/math/scalblnf.c
+++ Library/math/scalblnf.c
@@ -10,5 +10,30 @@ float scalblnf(float x, long int n)
         "fscale \n" \
         "fstp %0"
         : "=m"(x): "m"(x), "m"(n));
-    return x;
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.overflow == 0 && exceptions.underflow == 0)
+    {
+        return (long int)x;
+    }
+    // range error
+    else if(exceptions.overflow == 1)
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_OVERFLOW);
+        }
+        // In other case overflow is already set so we don't need to cover _math_errhandling == MATH_ERREXCEPT
+        return x > 0 ? HUGE_VALF : copysignf(HUGE_VALF, -1.0);
+    }
+    // range error (exceptions.underflow == 0)
+    else
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_UNDERFLOW);
+        }
+        return 0;
+    }
 }
