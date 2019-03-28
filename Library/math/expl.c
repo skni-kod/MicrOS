8b5791b14300781362f4107743330535cfccfcb7
AzuxDario <pawel_mibor95@tlen.pl>
2019-03-28 20:16:49 +0100

    Add error check into exp and ldexp

--- Library/math/expl.c
+++ Library/math/expl.c
@@ -2,5 +2,20 @@
 
 long double expl(long double x)
 {
-    return powl(M_E, x);
+    long double result = powl(M_E, x);
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.overflow == 0)
+    {
+        return result;
+    }
+    else
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_OVERFLOW);
+        }
+        // In other case overflow is already set so we don't need to cover _math_errhandling == MATH_ERREXCEPT
+        return x > 0 ? HUGE_VALL : copysignl(HUGE_VALL, -1.0);
+    }
 }
--- Library/math/ldexpf.c
+++ Library/math/ldexpf.c
@@ -2,11 +2,26 @@
 
 float ldexpf(float x, int exp)
 {
+    float result;
     __asm__ (
         "fild %2 \n" \
         "fld %1 \n" \
         "fscale \n" \
         "fstp %0"
-        : "=m"(x): "m"(x), "m"(exp));
-    return x;
+        : "=m"(result): "m"(x), "m"(exp));
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.overflow == 0)
+    {
+        return result;
+    }
+    else
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_OVERFLOW);
+        }
+        // In other case overflow is already set so we don't need to cover _math_errhandling == MATH_ERREXCEPT
+        return x > 0 ? HUGE_VALF : copysignf(HUGE_VALF, -1.0);
+    }
 }
