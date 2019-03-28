a4c144890063c0f1367b0e6b591a687f762bb081
tearth <tearthdev@gmail.com>
2019-03-28 23:34:40 +0100

    Add temporary strxfrm implementation.

--- Library/string/strxfrm.c
+++ Library/string/strxfrm.c
@@ -2,5 +2,7 @@
 
 size_t strxfrm(char *destination, const char *source, size_t num)
 {
-    return 0;
+    // NOTE: We use strcpy instead of something which considers locale settings. Will be changed later.
+    strcpy(destination, source);
+    return strlen(destination);
 }
