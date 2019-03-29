5e97c5146f9d3083f597f6a0859f22bfd1716794
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-03-29 01:11:03 +0100

    Created skeleton of setModeForDriver

--- OS/Kernel/Drivers/VGA/genericvga.c
+++ OS/Kernel/Drivers/VGA/genericvga.c
@@ -17,10 +17,32 @@ void driverInit()
 int16_t setVideoModeVGA(uint16_t mode){
     switch(mode)
     {
+        case 0x03:
+            turnOffBuffer();
+        case 0x04:
+            turnOffBuffer();
+        case 0x05:
+            turnOffBuffer();
+        case 0x06:
+            turnOffBuffer();
+        case 0x0D:
+            turnOffBuffer();
+        case 0x0E:
+            turnOffBuffer();
+        case 0x0F:
+            turnOffBuffer();
+        case 0x10:
+            turnOffBuffer();
+        case 0x11:
+            turnOffBuffer();
+        case 0x12:
+            turnOffBuffer();
         case 0x13:
+            turnOffBuffer();
             if(setMode13H() != 0x13) return -1;
-            
             return 0x13;
+        case 0x69:
+            turnOffBuffer();
         default: return -1;
     }
 }
