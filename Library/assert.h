37bc91a040a8abf530a4ffaee68d9183b5f92bc2
tearth <tearthdev@gmail.com>
2019-03-30 13:42:38 +0100

    Replace fputs with fprintf in assert function.

--- Library/assert.h
+++ Library/assert.h
@@ -5,11 +5,11 @@
 
 // TODO: add fprintf in format "Assertion failed: expression, file filename, line line number"
 #define STR(x) #x
-#define assert(expression)                   \
-    if (!(expression))                       \
-    {                                        \
-        fputs("Assertion failed: ", stderr); \
-        abort();                             \
+#define assert(expression)                                                      \
+    if (!(expression))                                                          \
+    {                                                                           \
+        fprintf(stderr, "Assertion failed: %s at line %d", __FILE__, __LINE__); \
+        abort();                                                                \
     }
 
 #endif
