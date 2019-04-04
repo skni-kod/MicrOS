b430f81efcac34a84b90ef5c2797b2cf601d3051
tearth <tearthdev@gmail.com>
2019-04-05 00:26:25 +0200

    Add tm structure.

--- Library/time.h
+++ Library/time.h
@@ -7,7 +7,22 @@
 #include "micros.h"
 
 typedef uint32_t clock_t;
+typedef uint64_t time_t;
+
+typedef struct tm
+{
+    uint32_t tm_sec;
+    uint32_t tm_min;
+    uint32_t tm_hour;
+    uint32_t tm_mday;
+    uint32_t tm_mon;
+    uint32_t tm_year;
+    uint32_t tm_wday;
+    uint32_t tm_yday;
+    uint32_t tm_isdst;
+} tm;
 
 clock_t clock();
+time_t time (time_t* timer);
 
 #endif
