1cd1925dc6dac405946293ef39bfd0fb75735bf9
AzuxDario <pawel_mibor95@tlen.pl>
2019-04-04 20:12:33 +0200

    Add error handling to current error and gamma functions

--- Library/math/erfc.c
+++ Library/math/erfc.c
@@ -3,4 +3,14 @@
 double erfc(double x)
 {
     return 1 - erf(x);
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.underflow == 1)
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_UNDERFLOW);
+        }
+        return 0;
+    }
 }
--- Library/math/erfcf.c
+++ Library/math/erfcf.c
@@ -3,4 +3,14 @@
 float erfcf(float x)
 {
     return 1 - erff(x);
+    fexcept_t exceptions = __FPU_read_status_word();
+    if(exceptions.underflow == 1)
+    {
+        if(_math_errhandling == MATH_ERRNO)
+        {
+            errno = ERANGE;
+            feclearexcept(FE_UNDERFLOW);
+        }
+        return 0;
+    }
 }
