cd109838a271198989ab2ed4e0113b4a09393efa
tearth <tearthdev@gmail.com>
2019-04-05 01:15:40 +0200

    Add time function.

--- Library/time.c
+++ Library/time.c
@@ -23,3 +23,19 @@ time_t mktime(tm * timeptr)
            (timeptr->tm_min * 60) +
            (timeptr->tm_sec);
 }
+
+time_t time(time_t* timer)
+{
+    micros_rtc_time rtc_time;
+    micros_rtc_read_time(&rtc_time);
+    
+    tm time;
+    time.tm_year = rtc_time.year;
+    time.tm_mon = rtc_time.month;
+    time.tm_mday = rtc_time.day;
+    time.tm_hour = rtc_time.hour;
+    time.tm_min = rtc_time.minute;
+    time.tm_sec = rtc_time.second;
+    
+    return mktime(&time);
+}
