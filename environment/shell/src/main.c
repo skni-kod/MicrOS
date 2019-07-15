0bc6be48aacbe0bf5065970ae89fac5dec362b82
tearth <tearthdev@gmail.com>
2019-07-15 14:15:43 +0200

    Add primitive shell.

--- environment/shell/src/main.c
+++ environment/shell/src/main.c
@@ -1,27 +1,23 @@
 #include <micros.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    micros_process_set_current_process_name("SHUKA");
-    while (1)
+    micros_process_set_current_process_name("SHELL");
+    printf("MicrOS Shell v1.0\n");
+    printf("Type path to execute an application\n");
+    
+    while(1)
     {
-        uint32_t now = micros_timer_get_system_clock();
-        while (now + 100 >= micros_timer_get_system_clock())
-            ;
-
-        /*
-        micros_keyboard_scan_ascii_pair pair;
-        micros_keyboard_wait_for_key_press(&pair);
-
-        char b[16];
-        micros_console_print_string("ASCII: ");
-        micros_console_print_string(itoa(pair.ascii, b, 10));
-        micros_console_print_string(", code: ");
-        micros_console_print_string(itoa(pair.scancode, b, 10));
-        micros_console_print_string("\n");*/
-
-        micros_process_current_process_sleep(200);
+        char buffer[64];
+        
+        printf("> ");
+        gets(buffer);
+        
+        uint32_t child_process_id = micros_process_start_process(buffer, "", true, true);
+        micros_process_wait_for_process(child_process_id);
     }
+    
     return 0;
 }
