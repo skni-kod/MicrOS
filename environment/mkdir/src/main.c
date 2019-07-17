6f5f9cff279572592316013ab9dc88e7a8bd046f
tearth <tearthdev@gmail.com>
2019-07-17 19:29:11 +0200

    Add "MKDIR" app.

---
+++ environment/mkdir/src/main.c
@@ -0,0 +1,44 @@
+#include <micros.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void reduce_slashes(char *path);
+
+int main(int argc, char *argv[])
+{
+    micros_process_set_current_process_name("MKDIR");
+    if(argc <= 3)
+    {
+        printf("Not enough parameters\n");
+        return -1;
+    }
+
+    char *current_dir = argv[1];
+    char *name = argv[2];
+    
+    char final_path[64];
+    sprintf(final_path, "%s/%s", current_dir, name);
+    
+    reduce_slashes(final_path);
+    micros_filesystem_create_directory(final_path);
+    
+    return -1;
+}
+
+void reduce_slashes(char *path)
+{
+    int length = strlen(path);
+    int i = 0;
+    
+    while (i < length - 1)
+    {
+        if(path[i] == '/' && path[i + 1] == '/')
+        {
+            memmove(path + i, path + i + 1, 63 - i);
+        }
+        else
+        {
+            i++;
+        }
+    }
+}
