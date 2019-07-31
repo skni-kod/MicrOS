0abd700088797c2bd11b428d71bcdf90cf2090c8
tearth <tearthdev@gmail.com>
2019-07-31 17:21:17 +0200

    Remove my cute optimization which killed OS on VirtualBox.

--- library/src/string/memcpy.c
+++ library/src/string/memcpy.c
@@ -2,23 +2,13 @@
 
 void *memcpy(void *destination, const void *source, size_t size)
 {
-    uint64_t *long_destination_ptr = destination;
-    const uint64_t *long_source_ptr = source;
+    char *destinatino_ptr = destination;
+    char *source_ptr = source;
 
-    while(size >= 8)
+    for(int i = 0; i < size; i++)
     {
-        *long_destination_ptr++ = *long_source_ptr++;
-        size -= 8;
+        destinatino_ptr[i] = source_ptr[i];
     }
     
-    uint8_t *short_destination_ptr = (uint8_t *)long_destination_ptr;
-    const uint8_t *short_source_ptr = (const uint8_t *)long_source_ptr;
-
-    while(size > 0)
-    {
-        *short_destination_ptr++ = *short_source_ptr++;
-        size--;
-    }
-
     return destination;
 }
