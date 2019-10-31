2a7c611d57e52bb80a1fa66f17f7366417ecded4
tearth <tearthdev@gmail.com>
2019-10-31 15:03:38 +0100

    Add support for generic vga in the library.

---
+++ library/src/micros/micros_generic_vga.c
@@ -0,0 +1,16 @@
+#include "micros_generic_vga.h"
+
+void micros_generic_vga_set_video_mode(uint16_t mode)
+{
+    micros_interrupt_1a(0x17, (uint32_t)mode);
+}
+
+bool micros_generic_vga_is_text_mode()
+{
+    return micros_interrupt_0a(0x18);
+}
+
+uint16_t micros_generic_vga_get_current_video_mode()
+{
+    return micros_interrupt_0a(0x19);
+}
