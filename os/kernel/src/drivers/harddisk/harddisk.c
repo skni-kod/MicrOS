f38c1e55ab054f70f930e88e63abeb52c0db14e8
AzuxDario <pawel_mibor95@tlen.pl>
2019-11-03 20:43:33 +0100

    Finished function to detect drive

--- os/kernel/src/drivers/harddisk/harddisk.c
+++ os/kernel/src/drivers/harddisk/harddisk.c
@@ -1,7 +1,7 @@
 #include "harddisk.h"
 
 
-bool check_harddisk_presence(uint8_t master, uint8_t bus)
+uint8_t check_harddisk_presence(uint8_t master, uint8_t bus)
 {
     uint16_t io_port = 0;
     uint16_t message_to_drive = 0;
@@ -17,20 +17,20 @@ bool check_harddisk_presence(uint8_t master, uint8_t bus)
     }
     else
     {
-        return;
+        return -2;
     }
 
     // Set drive
     switch (master)
     {
-    case 0:
-        message_to_drive = 0xB0;
-        break;
     case 1:
         message_to_drive = 0xA0;
         break;
+    case 2:
+        message_to_drive = 0xB0;
+        break;
     default:
-        return false;
+        return -2;
     }
 
     // Send message to drive
@@ -57,11 +57,41 @@ bool check_harddisk_presence(uint8_t master, uint8_t bus)
     // If the value read is 0, the drive does not exist.
     if(value == 0)
     {
-        return false;
-    }
-    // For any other value: poll the Status port (0x1F7) until bit 7 (BSY, value = 0x80) clears.
+        return 0;
+    }  
     else
-    {
-        return true;
+    {   // For any other value: poll the Status port (0x1F7) until bit 7 (BSY, value = 0x80) clears.
+        for(;;)
+        {
+            harddisk_io_control_status_register *status = (harddisk_io_control_status_register *) &value;
+            if(status->busy == 0)
+            {
+                // Otherwise, continue polling one of the Status ports until bit 3 (DRQ, value = 8) sets, or until bit 0 (ERR, value = 1) sets.
+                for(;;)
+                {
+                    value = io_in_byte(0x1F7);
+                    status = (harddisk_io_control_status_register *) &value;
+                    if(status->has_pio_data_to_transfer_or_ready_to_accept_pio_data == 1)
+                    {
+                        return 1;
+                    }
+                    else if(status->error_occurred)
+                    {
+                        return -1;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                // Because of some ATAPI drives that do not follow spec, at this point you need to check the LBAmid and LBAhi ports (0x1F4 and 0x1F5)
+                // to see if they are non-zero. If so, the drive is not ATA, and you should stop polling.
+                if(io_in_word(io_port + harddisk_io_cylinder_low_register_offset) != 0 || io_in_word(io_port + harddisk_io_cylinder_high_register_offset) != 0)
+                {
+                    return 0;
+                }
+            }
+            value = io_in_byte(0x1F7);
+        }
     }
 }
--- os/kernel/src/drivers/harddisk/harddisk.h
+++ os/kernel/src/drivers/harddisk/harddisk.h
@@ -9,10 +9,10 @@
 //! Check hard disk presence in computer.
 /*!
     Check if given hard disk is installed in computer.
-    \param master Set 1 to check master, set 0 to check slave.
+    \param master Set 1 to check master, set 2 to check slave.
     \param bus Set 1 to check primary bus, set 2 to check secondary bus.
-    \return Existence of disk.
+    \return 0 no disk, 1 disk present, -1 disk present but ERR set, -2 wrong parameters.
 */
-bool check_harddisk_presence(uint8_t master, uint8_t bus);
+uint8_t check_harddisk_presence(uint8_t master, uint8_t bus);
 
 #endif HARDDISK_H
