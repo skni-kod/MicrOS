b372c7aedc146469084095d19e4b8c53662fef93
tearth <tearthdev@gmail.com>
2019-11-05 15:53:45 +0100

    Add comments to assert functions.

--- library/src/assert.h
+++ library/src/assert.h
@@ -3,6 +3,11 @@
 
 #include "stdio.h"
 
+//! Check if value is upper alphanumeric
+/*!  
+    If the argument expression of this macro with functional form compares equal to zero (i.e., the expression is false), a message is written to the standard error device and abort is called, terminating the program execution.
+    \param Expression to be evaluated. If this expression evaluates to 0, this causes an assertion failure that terminates the program..
+*/
 #define assert(expression)                                                                          \
     if (!(expression))                                                                              \
     {                                                                                               \
