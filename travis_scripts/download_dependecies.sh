88bbc000cb67a34527350e72da38958dec56b577
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-11-30 01:10:12 +0100

    Update download_dependecies.sh

--- travis_scripts/download_dependecies.sh
+++ travis_scripts/download_dependecies.sh
@@ -1,4 +1,4 @@
 wget http://newos.org/toolchains/i386-elf-4.9.1-Linux-x86_64.tar.xz
 tar xf i386-elf-4.9.1-Linux-x86_64.tar.xz
-ls i386-elf-4.9.1-Linux-x86_64.tar.xz
+ls i386-elf-4.9.1-Linux-x86_64
 pwd
