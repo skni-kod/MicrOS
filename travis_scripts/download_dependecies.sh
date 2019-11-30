246265432d76ecf322b0801db2b6c8cc898d4754
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-11-30 01:33:31 +0100

    Update download_dependecies.sh

--- travis_scripts/download_dependecies.sh
+++ travis_scripts/download_dependecies.sh
@@ -6,5 +6,6 @@ wget https://github.com/jaenek/MicrOS-DevTools/archive/v2.0.tar.gz
 tar xf v2.0.tar.gz
 mv v2.0.tar.gz Scripts
 ls
-rm i386-elf-4.9.1-Linux-x86_64.tar.xz v2.0.tar.gz
+rm i386-elf-4.9.1-Linux-x86_64.tar.xz
 pwd
+echo $TRAVIS_BUILD_DIR
