be5aba6dc81d93b387a7963af73d2dd0b1c32695
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-11-30 01:18:49 +0100

    Download Dev Tools

--- travis_scripts/download_dependecies.sh
+++ travis_scripts/download_dependecies.sh
@@ -1,4 +1,9 @@
 wget http://newos.org/toolchains/i386-elf-4.9.1-Linux-x86_64.tar.xz
 tar xf i386-elf-4.9.1-Linux-x86_64.tar.xz
-ls i386-elf-4.9.1-Linux-x86_64
+mv i386-elf-4.9.1-Linux-x86_64 gcc_cross
+ls gcc_cross
+wget https://github.com/jaenek/MicrOS-DevTools/archive/v2.0.tar.gz
+tar xf v2.0.tar.gz
+mv v2.0 Scripts
+rm i386-elf-4.9.1-Linux-x86_64.tar.xz v2.0.tar.gz
 pwd
