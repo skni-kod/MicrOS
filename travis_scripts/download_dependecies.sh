7743843babd9cb56fa29b2299a09fc016cd900b1
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2019-11-30 01:22:15 +0100

    Update download_dependecies.sh

--- travis_scripts/download_dependecies.sh
+++ travis_scripts/download_dependecies.sh
@@ -4,7 +4,7 @@ mv i386-elf-4.9.1-Linux-x86_64 gcc_cross
 ls gcc_cross
 wget https://github.com/jaenek/MicrOS-DevTools/archive/v2.0.tar.gz
 tar xf v2.0.tar.gz
-mv v2.0 Scripts
+mv v2.0.tar.gz Scripts
 ls
 rm i386-elf-4.9.1-Linux-x86_64.tar.xz v2.0.tar.gz
 pwd
