6dab94ee3f9def593bf63e27473edaba4a950990
Tearth <tearthdev@gmail.com>
2020-06-12 23:34:34 +0200

    Improve GDB stub

--- os/kernel/src/gdb/gdb_stub_micros.c
+++ os/kernel/src/gdb/gdb_stub_micros.c
@@ -1,14 +1,17 @@
-void putDebugChar()
+#include "cpu/idt/idt.h"
+#include "drivers/serial/serial.h"
+
+int getDebugChar()
 {
-    
+    return serial_receive(COM1_PORT);
 }
 
-int getDebugChar(int c)
+void putDebugChar(int c)
 {
-    return 0;
+    serial_send(COM1_PORT, c);
 }
 
 void exceptionHandler(int exception_number, void *exception_address)
 {
-    
+    idt_attach_exception_handler(exception_number, exception_address);
 }
