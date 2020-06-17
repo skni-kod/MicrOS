39036df2a5a3de89c38a7f51fb68427dc3a9dcdb
AzuxDario <pawel_mibor95@tlen.pl>
2020-06-17 02:40:35 +0200

    Added remove and rename functions

--- library/src/stdio.h
+++ library/src/stdio.h
@@ -121,6 +121,26 @@ FILE *stderr;
 extern "C" {
 #endif
 
+//! Remove file.
+/*
+    Deletes the file whose name is specified in \p filename.    
+    \param filename C string containing the name of the file to be deleted.<br>
+    Attention: MicrOS requires full file path. Use \p main parameter with path if you want remove file from program folder.
+    \return If the file is successfully deleted, a zero value is returned. On failure, a nonzero value is returned.
+*/
+int remove(const char *filename);
+
+//! Rename file.
+/*
+    Changes the name of the file or directory specified by \p oldname to \p newname.    
+    \param oldname C string containing the name of an existing file to be renamed and/or moved.<br>
+    Attention: MicrOS requires full file path. Use \p main parameter with path if you want work on file from program folder.
+    \param newname C string containing the name of an existing file to be renamed and/or moved.<br>
+    Attention: MicrOS requires here only new filename without leading path.
+    \return If the file is successfully renamed, a zero value is returned. On failure, a nonzero value is returned.
+*/
+int rename(const char *oldname, const char *newname);
+
 //! Open file.
 /*!
     Opens the file whose name is specified in the parameter filename and associates it with a stream that can be identified in future operations by the FILE pointer returned. The returned stream is fully buffered by default. 
