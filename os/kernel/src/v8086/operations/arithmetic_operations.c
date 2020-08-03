da7cd2cb46303f9bb75854d2aba6166b04fe3dd0
SzateX <mistrzswiata.salata@gmail.com>
2020-08-03 23:10:55 +0200

    Cleaned some warnings.

--- os/kernel/src/v8086/operations/arithmetic_operations.c
+++ os/kernel/src/v8086/operations/arithmetic_operations.c
@@ -280,11 +280,11 @@ int16_t perform_sar(v8086* machine, void* dest, uint8_t arg, uint8_t width)
     else if(width == 32)
             __asm__ __volatile__("sarl %%cl, %%eax; pushfw; pop %%bx;" : "=b" (temp_flags), "=a" (*((uint32_t*) dest)) : "a" (*((uint32_t*) dest)), "c" (arg));
     else return -1;
-    if(arg == 1) bit_write(machine->regs.w.flags, 1<<OVERFLOW_FLAG_BIT, bit_get(temp_flags, 1<<OVERFLOW_FLAG_BIT) != 0);
-    bit_write(machine->regs.w.flags, 1<<CARRY_FLAG_BIT, bit_get(temp_flags, 1<<CARRY_FLAG_BIT) != 0);
-    bit_write(machine->regs.w.flags, 1<<SIGN_FLAG_BIT, bit_get(temp_flags, 1<<SIGN_FLAG_BIT) != 0);
-    bit_write(machine->regs.w.flags, 1<<ZERO_FLAG_BIT, bit_get(temp_flags, 1<<ZERO_FLAG_BIT) != 0);
-    bit_write(machine->regs.w.flags, 1<<PARITY_FLAG_BIT, bit_get(temp_flags, 1<<PARITY_FLAG_BIT) != 0);
+    if(arg == 1) bit_write(machine->regs.w.flags, 1u<<OVERFLOW_FLAG_BIT, bit_get(temp_flags, 1u<<OVERFLOW_FLAG_BIT) != 0);
+    bit_write(machine->regs.w.flags, 1u<<CARRY_FLAG_BIT, bit_get(temp_flags, 1u<<CARRY_FLAG_BIT) != 0);
+    bit_write(machine->regs.w.flags, 1u<<SIGN_FLAG_BIT, bit_get(temp_flags, 1u<<SIGN_FLAG_BIT) != 0);
+    bit_write(machine->regs.w.flags, 1u<<ZERO_FLAG_BIT, bit_get(temp_flags, 1u<<ZERO_FLAG_BIT) != 0);
+    bit_write(machine->regs.w.flags, 1u<<PARITY_FLAG_BIT, bit_get(temp_flags, 1u<<PARITY_FLAG_BIT) != 0);
     return 0;
 }
 
