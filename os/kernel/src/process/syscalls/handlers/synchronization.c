ebe45a5277b6167a8869fb10ffad5ff8aedd30a2
AzuxDario <pawel_mibor95@tlen.pl>
2020-08-10 17:19:04 +0200

    Fix in syscalls

--- os/kernel/src/process/syscalls/handlers/synchronization.c
+++ os/kernel/src/process/syscalls/handlers/synchronization.c
@@ -2,30 +2,30 @@
 
 void syscall_binary_semaphore_create_named(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_create_named((char*)state->registers.eax);
+    state->registers.eax = binary_semaphore_create_named((char*)state->registers.ebx);
 }
 
 void syscall_binary_semaphore_acquire(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_acquire((binary_semaphore)state->registers.eax);
+    state->registers.eax = binary_semaphore_acquire((binary_semaphore)state->registers.ebx);
 }
 
 void syscall_binary_semaphore_try_acquire(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_try_acquire((binary_semaphore)state->registers.eax);
+    state->registers.eax = binary_semaphore_try_acquire((binary_semaphore)state->registers.ebx);
 }
 
 void syscall_binary_semaphore_release(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_release((binary_semaphore)state->registers.eax);
+    state->registers.eax = binary_semaphore_release((binary_semaphore)state->registers.ebx);
 }
 
 void syscall_binary_semaphore_destroy(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_destroy((binary_semaphore)state->registers.eax);
+    state->registers.eax = binary_semaphore_destroy((binary_semaphore)state->registers.ebx);
 }
 
 void syscall_binary_semaphore_available(interrupt_state *state)
 {
-    state->registers.eax = binary_semaphore_available((binary_semaphore)state->registers.eax);
+    state->registers.eax = binary_semaphore_available((binary_semaphore)state->registers.ebx);
 }
