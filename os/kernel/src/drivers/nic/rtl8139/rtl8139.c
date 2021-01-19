8494fc34c3b9c794333919d61201dd8558aec141
JakubPrzystasz <jakub.przystasz@gmail.com>
2021-01-19 23:26:09 +0100

    Add headers, add rtl8139_init, read_mac, get_mac

--- os/kernel/src/drivers/nic/rtl8139/rtl8139.c
+++ os/kernel/src/drivers/nic/rtl8139/rtl8139.c
@@ -1,2 +1,138 @@
 #include "rtl8139.h"
 
+pci_device pci_rtl8139_device = {0};
+rtl8139_dev_t rtl8139_device = {0};
+
+uint32_t current_packet_ptr;
+
+//! Transmit start address of descritor (device has 4 descriptors)
+uint8_t TSAD_array[4] = {0x20, 0x24, 0x28, 0x2C};
+
+//! Transmit status of descriptor
+uint8_t TSD_array[4] = {0x10, 0x14, 0x18, 0x1C};
+
+void rtl8139_init()
+{
+    //Prevent for re-init
+    if (pci_rtl8139_device.vendor_id != 0)
+    {
+        return;
+    }
+
+    // First get the network device using PCI
+    // PCI must be initialized!
+    for (int i = 0; i < pci_get_number_of_devices(); i++)
+    {
+        pci_device *dev = pci_get_device(i);
+        if (dev->vendor_id == DEVICE_VENDOR_ID && dev->device_id == DEVICE_ID)
+        {
+            pci_rtl8139_device = *dev;
+            break;
+        }
+    }
+
+    if (pci_rtl8139_device.vendor_id == 0)
+    {
+        logger_log_info("NIC not found\n");
+        return;
+    }
+
+    //Now setup registers, and memory
+    rtl8139_device.bar_type = pci_rtl8139_device.base_addres_0 & (0x1);
+    rtl8139_device.io_base = pci_rtl8139_device.base_addres_0 & (~0x3);
+    rtl8139_device.mem_base = pci_rtl8139_device.base_addres_0 & (~0xf);
+
+    rtl8139_device.tx_cur = 0;
+
+    uint32_t pci_command_reg = pci_rtl8139_device.command;
+    if (!(pci_command_reg & TOK))
+    {
+        pci_command_reg |= TOK;
+        io_out_byte(pci_rtl8139_device.command, pci_command_reg);
+    }
+
+    //Power on device
+    io_out_byte(rtl8139_device.io_base + CONFIG1, 0x0);
+
+    //Software reset
+    io_out_byte(rtl8139_device.io_base + CHIPCMD, 0x10);
+    while ((io_in_byte(rtl8139_device.io_base + CHIPCMD) & 0x10) != 0)
+    {
+        ;
+    }
+
+    //TODO: MEMORY ALLOCATION IS OK ?
+    // Allocate receive buffer
+    rtl8139_device.rx_buffer = heap_kernel_alloc(RX_BUFFER_SIZE, 0);
+    memset(rtl8139_device.rx_buffer, 0x0, RX_BUFFER_SIZE);
+    io_out_long(rtl8139_device.io_base + RXBUF, rtl8139_device.rx_buffer + 0xC0000000);
+
+    // Set TransmitterOK and ReceiverOK to HIGH
+    io_out_word(rtl8139_device.io_base + INTRMASK, 0x0005);
+
+    // (1 << 7) is the WRAP bit, 0xf is AB+AM+APM+AAP
+    io_out_long(rtl8139_device.io_base + RXCONFIG, 0xf | (1 << 7));
+
+    // Set ReceiverEnable and TransmitterEnable to HIGH
+    io_out_byte(rtl8139_device.io_base + CHIPCMD, 0x0C);
+
+    // Enable interruptions
+    uint32_t irq_num = pci_rtl8139_device.interrupt_line;
+    idt_attach_interrupt_handler(32 + irq_num, rtl8139_irq_handler);
+
+    rtl8139_read_mac_addr();
+
+    char logInfo[27] = "NIC MAC: __:__:__:__:__:__";
+    char numberBuffer[3];
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        itoa(rtl8139_device.mac_addr[i], numberBuffer, 16);
+        //Add leading zero
+        if (numberBuffer[1] == 0)
+        {
+            numberBuffer[1] = numberBuffer[0];
+            numberBuffer[0] = '0';
+        }
+        //Copy just 2 characters, we dont need ending 0
+        memcpy(logInfo + (9 + i * 3), numberBuffer, 2);
+    }
+
+    //Finally we can tell that - We did it!
+    logger_log_ok(logInfo);
+}
+
+void rtl8139_irq_handler(pci_in_data *data)
+{
+    //Get status of device
+    uint16_t status = io_in_word(rtl8139_device.io_base + INTRSTATUS);
+
+    if (status & TOK)
+    {
+        //logger_log_info("Packet sent");
+    }
+    if (status & ROK)
+    {
+        //logger_log_info("Packet received");
+        //receive_packet();
+    }
+
+    io_out_word(rtl8139_device.io_base + INTRSTATUS, 0x5);
+}
+
+void rtl8139_read_mac_addr()
+{
+    for (uint8_t i = 0; i < 6; i++)
+    {
+        rtl8139_device.mac_addr[i] = io_in_byte(rtl8139_device.io_base + i);
+    }
+}
+
+void rtl8139_get_mac_addr(uint8_t *buffer)
+{
+    if (buffer == 0)
+    {
+        return;
+    }
+
+    memcpy(buffer, rtl8139_device.mac_addr, 6);
+}
--- os/kernel/src/drivers/nic/rtl8139/rtl8139.h
+++ os/kernel/src/drivers/nic/rtl8139/rtl8139.h
@@ -7,5 +7,104 @@
 #include <stdlib.h>
 
 #define DEVICE_VENDOR_ID 0x10EC
+#define DEVICE_ID 0x8139
+
+#define RX_BUFFER_SIZE 8192 + 16
+
+#define CAPR 0x38
+#define RX_READ_POINTER_MASK (~3)
+#define ROK (1 << 0)
+#define RER (1 << 1)
+#define TOK (1 << 2)
+#define TER (1 << 3)
+#define TX_TOK (1 << 15)
+
+enum RTL8139_registers
+{
+    MAG0 = 0x00,      // Ethernet hardware address MAC
+    MAR0 = 0x08,      // Subnet mask
+    TXSTATUS0 = 0x10, // TX status (4x32bit)
+    TXADDR0 = 0x20,   // TX descriptors (4x32bit)
+    RXBUF = 0x30,
+    RXEARLYCOUNT = 0x34,
+    RXEARLYSTATUS = 0x36,
+    CHIPCMD = 0x37,
+    RXBUFPTR = 0x38,
+    RXBUFADDR = 0x3A,
+    INTRMASK = 0x3C,
+    INTRSTATUS = 0x3E,
+    TXCONFIG = 0x40,
+    RXCONFIG = 0x44,
+    TIMER = 0x48,    // A general-purpose counter
+    RXMISSED = 0x4C, // 24 bits valid, write clears
+    CFG9346 = 0x50,
+    CONFIG0 = 0x51,
+    CONFIG1 = 0x52,
+    FLASHREG = 0x54,
+    GPPINDATA = 0x58,
+    GPPINDIR = 0x59,
+    MII_SMI = 0x5A,
+    HLTCLK = 0x5B,
+    MULTIINTR = 0x5C,
+    TXSUMMARY = 0x60,
+    MII_BMCR = 0x62,
+    MII_BMSR = 0x64,
+    NWAYADVERT = 0x66,
+    NWAYLPAR = 0x68,
+    NWAYEXPANSION = 0x6A,
+
+    // Undocumented registers, but required for proper operation
+    FIFOTMS = 0x70, // FIFO Control and test
+    CSCR = 0x74,    // Chip Status and Configuration Register
+    PARA78 = 0x78,
+    PARA7C = 0x7C, // Magic transceiver parameter register
+};
+
+typedef struct tx_desc
+{
+    uint32_t phys_addr;
+    uint32_t packet_size;
+} tx_desc_t;
+
+typedef struct rtl8139_dev
+{
+    uint8_t bar_type;
+    uint16_t io_base;
+    uint32_t mem_base;
+    int eeprom_exist;
+    uint8_t mac_addr[6];
+    char *rx_buffer;
+    int tx_cur;
+} rtl8139_dev_t;
+
+//! rtl8139_init
+/*!
+    Initialize network card driver 
+    Requires initialized PCI driver
+*/
+void rtl8139_init();
+
+void rtl8139_send_packet(void *data, uint32_t len);
+
+void rtl8139_receive_packet();
+
+//! rtl8139_irq_handler
+/*!
+    Manages interruptions of device
+*/
+void rtl8139_irq_handler(pci_in_data *data);
+
+//! rtl8139_read_mac_addr
+/*!
+    Reads device mac address
+*/
+void rtl8139_read_mac_addr();
+
+//! rtl8139_get_mac_addr
+/*!
+    Puts to buffer mac address of device 
+    Requires buffer with size of (6 * uint8_t)
+*/
+void rtl8139_get_mac_addr(uint8_t *buffer);
 
 #endif
