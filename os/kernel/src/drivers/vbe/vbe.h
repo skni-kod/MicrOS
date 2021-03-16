4b3d087d04ac07c9deda9496dd28c6e204e36c9a
xBLACKxSNAKEx <damianpietryka2003@gmail.com>
2021-03-16 19:21:18 +0100

    Revert "Added VBE display window control function"
    This reverts commit 9562ab725e60c7d7294f741a5c0478bebb517378.

--- os/kernel/src/drivers/vbe/vbe.h
+++ os/kernel/src/drivers/vbe/vbe.h
@@ -53,8 +53,5 @@ VBEStatus VBE_set_video_mode(uint16_t mode_number, bool clear_screen);
 VBEStatus VBE_get_current_video_mode(uint16_t* mode_number);
 VBEStatus VBE_return_save_restore_state_buffer_size(uint16_t requested_states, uint16_t* buffer_block_number);
 VBEStatus VBE_save_restore_state(bool save, uint16_t requested_states, uint16_t buffer_pointer);
-VBEStatus VBE_get_display_window_control_16bit(uint8_t window_number, uint16_t* window_mem_number);
-VBEStatus VBE_set_display_window_control_16bit(uint8_t window_number, uint16_t window_mem_number);
-VBEStatus VBE_set_display_window_control_32bit(uint8_t window_number, uint16_t window_mem_number, uint16_t memory_selector);
 
 #endif
