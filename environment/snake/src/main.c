2ced5076ea36c415daa0d5f1d14af7814f0195e4
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2021-09-14 21:32:12 +0200

    Clearing debug things in snake

--- environment/snake/src/main.c
+++ environment/snake/src/main.c
@@ -1,20 +1,10 @@
 #include "core/core.h"
-#include <micros/micros_console.h>
 
 char current_partition;
-
-char dupa[320*200];
 
 int main(int argc, char *argv[])
 {
     current_partition = argv[1][0];
-    //core_run();
-    for(int i = 0 ; i < 320 * 200; i++)
-    {
-        dupa[i] = 40;
-    }
-    micros_console_set_video_mode(0x13);
-    micros_console_copy_from_buffer(dupa, 320*200);
-    while(1);
+    core_run();
     return 0;
 }
