e324178f55be17f4d8b37437dac46e84f62e3a13
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2021-09-18 02:23:58 +0200

    Bugfix in DAL and Generic Driver causes pagefault when diffrent modes on terminals. - Removed setting functions for external buffer in video card driver

--- os/kernel/src/drivers/vga/modes/mode_02h/mode_02h.c
+++ os/kernel/src/drivers/vga/modes/mode_02h/mode_02h.c
@@ -395,18 +395,6 @@ int8_t mode02h_set_mode()
     video_card_set_draw_circle_func(mode02h_draw_circle);
     video_card_set_draw_rectangle_func(mode02h_draw_rectangle);
     video_card_set_clear_screen_func(mode02h_clear_screen);
-	video_card_set_swap_external_buffer(&mode02h_swap_external_buffer);
-
-	video_card_set_print_char_external_buffer(mode02h_print_char_external_buffer);
-	video_card_set_print_char_color_external_buffer(mode02h_print_char_color_external_buffer);
-	video_card_set_print_string_external_buffer(mode02h_print_string_external_buffer);
-	video_card_set_print_string_color_external_buffer(mode02h_print_string_color_external_buffer);
-	video_card_set_set_char_external_buffer(mode02h_set_char_external_buffer);
-	video_card_set_get_char_external_buffer(mode02h_get_char_external_buffer);
-	video_card_set_set_color_external_buffer(mode02h_set_color_external_buffer);
-	video_card_set_get_color_external_buffer(mode02h_get_color_external_buffer);
-	video_card_set_set_char_and_color_external_buffer(mode02h_set_char_and_color_external_buffer);
-	video_card_set_get_char_and_color_external_buffer(mode02h_get_char_and_color_external_buffer);
     return 0x02;
 }
 
