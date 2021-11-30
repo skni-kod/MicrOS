b0ab1fd7e215834d3069f2592cf4df29ae8f7e39
MatiF100 <mateuszfesz@wp.pl>
2021-11-30 01:10:36 +0100

    Created basic implementation of ZLIB library

---
+++ environment/azlib/src/main.c
@@ -0,0 +1,14 @@
+#include <zlib.h>
+#include <micros.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main(int argc, char *argv[])
+{
+;  
+int dupa = 1;
+}
+
--- library/src/string.h
+++ library/src/string.h
@@ -5,7 +5,7 @@
 #include "stddef.h"
 
 //! Unsigned integral type.
-typedef size_t uint32_t;
+//typedef size_t uint32_t;
 
 #ifdef __cplusplus
 extern "C" {
