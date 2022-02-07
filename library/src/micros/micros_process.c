e78b756a0993fbc64bcaf9e7de4d9998233beb82
Mateusz Wojtoń <theminusoid@gmail.com>
2022-02-07 19:33:51 +0100

    Applied requested changes for PR #111

--- library/src/micros/micros_process.c
+++ library/src/micros/micros_process.c
@@ -12,7 +12,7 @@ uint32_t micros_process_get_processes_count()
 
 void micros_process_get_current_process_info(micros_process_user_info *user_info)
 {
-    micros_interrupt_1a(0x7020, (uint32_t)user_info);
+    micros_interrupt_1a(0x0702, (uint32_t)user_info);
 }
 
 bool micros_process_get_process_info(uint32_t id, micros_process_user_info *user_info)
