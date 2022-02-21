c7d522d4a86478ee5426e97979d736ab1c1ed3da
Jakub Przystasz <jakub.przystasz@gmail.com>
2022-02-21 18:48:00 +0100

    Revert 'add pagaing and C linker

--- os/bootloader-fat12/kernel_loader/src/loader.asm
+++ os/bootloader-fat12/kernel_loader/src/loader.asm
@@ -1,4 +1,5 @@
 [BITS 16]
+[ORG 0xF000]
 jmp loaderMain
 
 ;GDT
@@ -339,16 +340,9 @@ load_memory_map:
     ret
 
 [BITS 32]
-extern loader_main
-
-%define PAGE_DIRECTORY_BASE 0x00006000
-%define PAGE_TABLES_BASE    0x01100000
-%define PAGES_COUNT         6
-
 main_protected_area:
     ;call protstr
     cli
-    ;hlt
     ; Set data and stack segments to the third GDI descriptor
     mov ax, 0x10
     mov ds, ax
@@ -357,36 +351,11 @@ main_protected_area:
     mov gs, ax
     mov ss, ax
 
-    call clear_page_directory
-    ;call protstr
-    ;cli
-    ;hlt
-    
-    call clear_page_tables
-
-    ;call protstr
-    ;cli
-    ;hlt
-
-    call create_page_directory
-    call create_identity_page_table
-    call create_kernel_page_table
-    call enable_paging  
-    
-    ; Set new stack with virtual address
-    mov eax, 0xC1100000
-    mov esp, eax
-
-    ; Init FPU
-    finit
-
     mov esi, protected_in
     call protstr
-
-    call loader_main
-
-    ; Something went wrong, but no problem!
-    JMP $
+    
+    hlt
+    jmp $
 
 ; temporarily it is going to print at the beginning!
 ; Function for displaying debug text in protected mode
@@ -415,148 +384,6 @@ protstr_loop:
     add ebx, 2
     jmp protstr_loop
 protstr_done:
-    ret
-
-; Output: nothing
-clear_page_directory:
-    ; Index
-    mov eax, 0
-
-    clear_page_directory_loop:
-    ; Clear entry
-    mov [PAGE_DIRECTORY_BASE + eax], byte 0
-
-    ; Increment index
-    inc eax
-
-    ; Leave loop if we cleared all entries
-    cmp eax, 0x1000
-    jl clear_page_directory_loop
-
-    ret
-
-; Input: nothing
-; Output: nothing
-clear_page_tables:
-    ; Index
-    mov eax, 0
-
-    clear_page_tables_loop:
-    ; Clear entry
-    mov [PAGE_TABLES_BASE + eax], byte 0
-
-    ; Increment index
-    inc eax
-
-    ; Leave loop if we cleared all entries
-    cmp eax, 0x400000
-    jl clear_page_tables_loop
-
-    ;;call protstr
-    ;;cli
-    ;;hlt
-
-    ret
-
-; Input: nothing
-; Output: nothing
-create_page_directory:
-    ; Page definition macro
-    %macro page_directory_definition 3
-    mov eax, %1 + 3
-    mov [%2 + %3], eax
-    %endmacro
-    
-    ; Add temporary identity entry (physical address: 0x00000000, virtual address: 0x00000000, 24 MB)
-    %assign i 0 
-    %rep    PAGES_COUNT 
-    page_directory_definition PAGE_TABLES_BASE + (i * 0x1000), PAGE_DIRECTORY_BASE, i * 4
-    %assign i i+1 
-    %endrep
-
-    ; Add kernel entry (physical address: 0x00000000, virtual address: 0xC0000000, 24 MB)
-    %assign i 0 
-    %rep    PAGES_COUNT 
-    page_directory_definition PAGE_TABLES_BASE + 0x00300000 + (i * 0x1000), PAGE_DIRECTORY_BASE + 0xC00, i * 4
-    %assign i i+1 
-    %endrep
-
-    ret
-
-; Input: nothing
-; Output: nothing
-create_identity_page_table:
-    ; Entry index
-    mov eax, 0
-
-    ; Entry physical address
-    mov ebx, 0
-
-    fill_identity_page_table_loop:
-    ; Set flags: read/write, present
-    mov ecx, ebx
-    or ecx, 3
-    
-    ; Set entry
-    mov [PAGE_TABLES_BASE + eax*4], ecx
-
-    ; Go to the next entry
-    add ebx, 0x1000
-    inc eax
-
-    ; Leave loop if we filled all entries for the first 6 megabytes
-    cmp eax, PAGES_COUNT * 0x1000
-    jl fill_identity_page_table_loop
-
-    ret
-
-; Input: nothing
-; Output: nothing
-create_kernel_page_table:
-    ; Entry index
-    mov eax, 0
-
-    ; Entry physical address
-    mov ebx, 0
-
-    fill_kernel_page_table_loop:
-    ; Set flags: read/write, present
-    mov ecx, ebx
-    or ecx, 3
-    
-    ; Set entry
-    mov [PAGE_TABLES_BASE + 0x00300000 + eax*4], ecx
-
-    ; Go to the next entry
-    add ebx, 0x1000
-    inc eax
-
-    ; Leave loop if we filled all entries for the first megabyte
-    cmp eax, PAGES_COUNT * 0x1000
-    jl fill_kernel_page_table_loop
-
-    ret
-
-; Input: nothing
-; Output: nothing
-enable_paging:
-    ; Set address of the directory table
-    mov eax, PAGE_DIRECTORY_BASE
-    mov cr3, eax
-
-    ; Enable paging
-    mov eax, cr0
-    or eax, 0x80000020
-    mov cr0, eax
-
-    jmp .branch
-    nop
-    nop
-    nop
-    nop
-    nop
-    .branch:
-
     ret
 
 ; 32bit globals
