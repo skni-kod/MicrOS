8ca2ce8fdf0ddd625d6fa8cc2dac2d930f854f1d
Mateusz Wojtoń <theminusoid@gmail.com>
2023-02-20 20:21:13 +0100

    Revert "Revert "Fixed PCI device listing.""

--- os/kernel/src/drivers/pci/pci_device.h
+++ os/kernel/src/drivers/pci/pci_device.h
@@ -1,14 +1,20 @@
 #ifndef PCI_DEVICE
 #define PCI_DEVICE
 
-typedef union pci_device
+/**
+ * Union with inner structure representing PCI configuration space.
+ * Stores all data from configuration space.
+ * Divided into 64 byte predefined header with first 16 bytes always the same.
+ * Further 192 bytes are device dependent and driver needs to work with these.
+ */
+typedef union pci_config
 {
-    uint8_t bits_8[64];
-    uint16_t bits_16[32];
-    //addinational 16 descriptors
-    uint32_t bits_32[32];
+    uint8_t bytes[256];
+    uint16_t words[128];
+    uint32_t dwords[64];
     struct
     {
+        //ALWAYS THE SAME
         uint16_t vendor_id;
         uint16_t device_id;
         uint16_t command;
@@ -21,6 +27,7 @@ typedef union pci_device
         uint8_t latency_timer;
         uint8_t header_type;
         uint8_t bist;
+        //DEPENDS ON HEADER TYPE
         uint32_t base_addres_0;
         uint32_t base_addres_1;
         uint32_t base_addres_2;
@@ -37,8 +44,39 @@ typedef union pci_device
         uint8_t interrupt_pin;
         uint8_t min_grant;
         uint8_t max_latency;
-        uint32_t descriptors[16];
+        union device_dependent
+        {
+            uint8_t bytes[192];
+            uint16_t words[96];
+            uint32_t dwords[48];
+        };
     };
-} __attribute__((packed)) pci_device;
+} __attribute__((packed)) pci_config;
+
+/**
+ * Structure representing device. Holds basic information about device including bus and device index and its configuration space.
+ */
+typedef struct pci_device
+{
+    uint16_t bus_id;
+    uint16_t dev_id;
+    uint8_t func_id;
+    pci_config config;
+} pci_device;
+
+typedef union pci_cmd_address_reg
+{
+    struct
+    {
+        uint32_t always_zero : 2;
+        uint32_t register_num : 6;
+        uint32_t function_num : 3;
+        uint32_t device_num : 5;
+        uint32_t bus_num : 8;
+        uint32_t reserved : 7;
+        uint32_t enable : 1;
+    };
+    uint32_t bits;
+} __attribute__((packed)) pci_cmd_address_reg;
 
 #endif
